cd8a408af1c304a2eb08a57a27e45592c81552a2
Lewis Truong <letruong@csumb.edu>
2019-10-08 13:36:28 -0700

    more dane subassets

---
+++ Finished_Projects/Metal Sphere/Metal Sphere/Assets/PlayerAssets/Scripts/thrownGrenadeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thrownGrenadeScript : MonoBehaviour
+{
+    public float detonationTimer = 3f;
+    private float detonationTimerRefrence = 0f;
+    private bool grenadeDetonated = false;
+
+    void Start()
+    {
+        detonationTimerRefrence = Time.time + detonationTimer;
+    }
+
+    void FixedUpdate()
+    {
+        if ((Time.time > detonationTimerRefrence) && !grenadeDetonated)
+        {
+            for (int i = 0; i < gameObject.transform.childCount; i++)
+                if (gameObject.transform.GetChild(i).gameObject.name == "Collider")
+                    gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            gameObject.GetComponent<AudioSource>().Play();
+            Destroy(gameObject, 1f);
+            grenadeDetonated = true;
+        }
+    }
+}
