835b8a3693baf8440a3d08d25bd302d3e5938f85
Lewis Truong <letruong@csumb.edu>
2019-11-13 11:08:46 -0800

    Blaze Squad - Fire Update
    Added Fire Script as well as a script to spread fire

---
+++ Current_Projects/BSW_Unity/Blaze Squad/Assets/Scripts/FireScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour
+{
+    public double fireDamage;
+    public double FuelReduction;
+    public double fireRange;
+    FireSpread parentData;
+    SphereCollider col;
+    void Start()
+    {
+        parentData = transform.GetComponentInParent<FireSpread>();
+        col = this.GetComponent<SphereCollider>();
+    }
+
+    void FixedUpdate()
+    {
+        if(parentData.fuel > 0)
+        {
+            parentData.fuel = parentData.fuel - FuelReduction;
+
+            if (parentData.fuel >= 51)
+            {
+                fireRange += 0.05;
+            }
+            else
+            {
+                fireRange -= 0.05;
+            }
+        }
+        
+
+        
+
+        //takes fuel, reduces it by FuelReduction and checks amount
+        //if fuel is approaching 51, increase damage and range
+        //if fuel is approaching 0, decrease its damage and range
+        if(fireRange <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            col.radius = (float)fireRange;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<FireSpread>() != null)
+        {
+            if (other.GetComponent<FireSpread>().burning == false)
+            {
+                other.GetComponent<FireSpread>().applyDamage(calculateDamage());
+            }
+        }
+    }
+
+    double calculateDamage()
+    {
+        if (parentData.fuel > 0)
+        {
+            if (parentData.fuel >= 51)
+            {
+                fireDamage += 0.05;
+            }
+            else
+            {
+                fireDamage -= 0.5;
+            }   
+        }
+
+        return fireDamage;
+    }
+}
---
+++ Current_Projects/BSW_Unity/Blaze Squad/Assets/Scripts/FireSpread.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpread : MonoBehaviour
+{
+
+    //=============================================
+    //Object Variables
+    //=============================================
+    public double health = 100;
+    public double waterShield = 100;
+    public double flammability = 0.75;
+    public double fuel = 100;
+    public bool burning = false;
+
+    void Start() { }
+
+    void FixedUpdate()
+    {
+        if(fuel <= 0)
+        {
+            burning = false;
+            this.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    //====================================================================================
+    //heatDamage - this script takes a damage value, and applies a series of calculations
+    //before subtracting the result from the objects health.
+    //====================================================================================
+    public void applyDamage(double damage)
+    {
+        
+        //1) Damage is compared to waterShield
+        //2) If waterShield is higher, reduce waterShield by damage and set damage to 0
+        //3) If damage is higher, then reduce damage by waterShield and set waterShield to 0
+        //--------------------------------------------------------------------------------
+        if (waterShield > damage)            //1)
+        {
+            waterShield = waterShield - damage;                 //2)
+            damage = 0;
+        }
+        else
+        {
+            damage = damage - waterShield;                      //3)
+            waterShield = 0;
+        }
+
+        //----------------------------------------------------------------------------------------
+        //1) Damage is calculated by multiplying the given value with the flammability of the object.
+        //2) The product is then subtracted from the health of the object.
+        //3) Object is set to burning if it's health has reached 0;
+        //-----------------------------------------------------------------------------------------
+        health = health - (damage * flammability);      //1) and 2)
+
+        if (health <= 0)                                //3)
+        {
+            burning = true;
+            this.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
