712779bd018fa5191dcad7c78e58588f2a06ad62
Lewis Truong <letruong@csumb.edu>
2019-11-14 02:16:19 -0800

    added movement fixed fires and added equipment

---
+++ Current_Projects/BSW_Unity/Blaze Squad/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public bool activeSelected;
+
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        if (activeSelected)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (!EventSystem.current.IsPointerOverGameObject())
+                    {
+                        print("Not on UI");
+                        navMeshAgent.SetDestination(hit.point);
+                    }
+                }
+            }
+        }
+    }
+}
